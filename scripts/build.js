ac607e5cb4a7f29af663fe0840d49d587629ddca
Nate Goldman <nate@ngoldman.me>
2015-07-08 19:17:40 -0700

    :sparkles: standardize

--- scripts/build.js
+++ scripts/build.js
@@ -1,33 +1,33 @@
-process.chdir(__dirname);
+process.chdir(__dirname)
 
-var fs = require('fs');
-var sass = require('node-sass');
-var CleanCSS = require('clean-css');
-var pkg = require('../package.json');
+var fs = require('fs')
+var sass = require('node-sass')
+var CleanCSS = require('clean-css')
+var pkg = require('../package.json')
 
 sass.render({
   file: '../source/top-bar.scss',
   success: buildCSS
-});
+})
 
 function buildCSS (result) {
-  var style = new CleanCSS().minify(result.css).styles;
-  var banner = '/* ' + pkg.name + ' v' + pkg.version  + ' - ' +
-                getDate() + ' - ' + pkg.homepage + ' */\n';
+  var style = new CleanCSS().minify(result.css).styles
+  var banner = '/* ' + pkg.name + ' v' + pkg.version + ' - ' +
+    getDate() + ' - ' + pkg.homepage + ' */\n'
   fs.writeFile('../top-bar.css', banner + style, function (err) {
-    if (err) { throw err; }
-    console.log('built top-bar.css');
-  });
+    if (err) throw err
+    console.log('built top-bar.css')
+  })
 }
 
 function getDate () {
-  var d = new Date();
-  var dd = d.getDate();
-  var mm = d.getMonth()+1;
-  var yyyy = d.getFullYear();
+  var d = new Date()
+  var dd = d.getDate()
+  var mm = d.getMonth() + 1
+  var yyyy = d.getFullYear()
 
-  if (dd<10) { dd='0'+dd; }
-  if (mm<10) { mm='0'+mm; }
+  if (dd < 10) dd = '0' + dd
+  if (mm < 10) mm = '0' + mm
 
-  return mm+'/'+dd+'/'+(''+yyyy).substr(2);
+  return mm + '/' + dd + '/' + ('' + yyyy).substr(2)
 }
