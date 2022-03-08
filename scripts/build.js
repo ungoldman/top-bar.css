b7b39c229f00b0cede6e43feba458f3a813f6436
Nate Goldman <ungoldman@gmail.com>
2022-03-08 10:37:42 -0800

    chore(maint): lint for latest standard

--- scripts/build.js
+++ scripts/build.js
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
 
-var fs = require('fs')
-var path = require('path')
-var CleanCSS = require('clean-css')
-var pkg = require('../package.json')
+const fs = require('fs')
+const path = require('path')
+const CleanCSS = require('clean-css')
+const pkg = require('../package.json')
 
 function buildCSS () {
-  var file = path.join(__dirname, '../top-bar.css')
-  var css = fs.readFileSync(file)
+  const file = path.join(__dirname, '../top-bar.css')
+  const css = fs.readFileSync(file)
 
-  var style = new CleanCSS().minify(css).styles
-  var banner = '/* ' + pkg.name + ' v' + pkg.version + ' - ' +
+  const style = new CleanCSS().minify(css).styles
+  const banner = '/* ' + pkg.name + ' v' + pkg.version + ' - ' +
     pkg.license + ' License - ' + pkg.homepage + ' */\n'
 
   fs.writeFile(file, banner + style, function (err) {
